0ab86bc3a46d42b14a18696d55c5c8b1bbba7842
Iqra Shaikh <shaikhiqra2210@gmail.com>
2026-09-11 18:57:37 +0530

    Update MongoDb Connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
 const express=require("express");
 const mongoose=require("mongoose");
 const cors=require("cors");
 const bookRoutes= require("./routes/bookRoutes");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb://127.0.0.1:27017/bookverse")
+
+mongoose.connect(process.env.MONGO_URI,{dbName:"BookVerse"})
 .then(()=>{
     console.log("MongoDB connected successfully");
 })
